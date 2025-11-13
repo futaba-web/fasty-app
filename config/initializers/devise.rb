0b086f3eaa703fbda8dfdc3c01bedfe83862a279
Futaba Tomiyama <runteqfuu0422@gmail.com>
2025-11-13 12:32:59 +0900

    Rubocop: devise 初期化ファイルのスタイルを修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -27,11 +27,11 @@ Devise.setup do |config|
   require "devise/orm/active_record"
 
   # == Authentication keys
-  config.case_insensitive_keys = [:email]
-  config.strip_whitespace_keys = [:email]
+  config.case_insensitive_keys = [ :email ]
+  config.strip_whitespace_keys = [ :email ]
 
   # == Session / Security
-  config.skip_session_storage = [:http_auth]
+  config.skip_session_storage = [ :http_auth ]
 
   # == Password hashing
   config.stretches = Rails.env.test? ? 1 : 12
@@ -58,7 +58,7 @@ Devise.setup do |config|
   # == Hotwire / Turbo
   config.responder.error_status    = :unprocessable_entity
   config.responder.redirect_status = :see_other
-  config.navigational_formats      = ["*/*", :html, :turbo_stream]
+  config.navigational_formats      = [ "*/*", :html, :turbo_stream ]
 
   # =====================================================
   # OmniAuth 用の共通ホスト
